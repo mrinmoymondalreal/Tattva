cc84c08aea521134881e70578319f50229b988a2
mrinmoymondalreal <mrinmoymondalreal+github@gmail.com>
2026-02-08 01:34:05 +0530

    Refactor font initialization in draw module; streamline font size handling and remove debug print statement

--- utils/draw.py
+++ utils/draw.py
@@ -1,8 +1,6 @@
 from .basic import getfontPath
 import pyray as ry
 
-print("fontPath", getfontPath("TIMES.TTF"))
-
 DEFAULTS = {
   "font": None,
   "font_size": 20,
@@ -10,18 +8,28 @@ DEFAULTS = {
   "color": ry.BLACK
 }
 
-def initDefaults():
-  DEFAULTS["font"] = ry.load_font_ex(getfontPath("TIMES.TTF"), 20, None, 0)
+font_sizes = {}
 
-def text(text, position, font_size = DEFAULTS["font_size"], color = DEFAULTS["color"], font = None, spacing = DEFAULTS["spacing"]):
+def initDefaults():
+  # DEFAULTS["font"] = ry.load_font_ex(getfontPath("TIMES.TTF"), 20, None, 0)
+  pass
+
+def initFontSizes(font_size=20):
+  font = None
   if font is None:
     font = DEFAULTS["font"]
+  if font_size not in font_sizes:
+    font = ry.load_font_ex(getfontPath("TIMES.TTF"), font_size, None, 0)
+    font_sizes[font_size] = font
+  return font_sizes[font_size]
+
+def text(text, position, font_size = DEFAULTS["font_size"], color = DEFAULTS["color"], font = None, spacing = DEFAULTS["spacing"]):
+  font = font or initFontSizes(font_size)
   ry.draw_text_ex(font, text, position, font_size, spacing, color)
 
 def rect(position, size, color = DEFAULTS["color"]):
   ry.draw_rectangle(position[0], position[1], size[0], size[1], color)
 
 def measureText(text, font_size = DEFAULTS["font_size"], font = None, spacing = DEFAULTS["spacing"]):
-  if font is None:
-    font = DEFAULTS["font"]
+  font = font or initFontSizes(font_size)
   return ry.measure_text_ex(font, text, font_size, spacing)
