a51c00fb64f9d34948d8ff36ff4999dcbeaf5935
mrinmoymondalreal <mrinmoymondalreal+github@gmail.com>
2026-02-08 01:34:13 +0530

    Refactor layout positioning logic in Layout class; enhance justify-content and align-items handling for improved layout behavior

--- layout/Layout.py
+++ layout/Layout.py
@@ -41,40 +41,51 @@ class Layout:
       pt, pl, pb, pr = self.parent.styles.padding
       parentFlexDirection = self.parent.styles.direction
       justifyContent = self.parent.styles.justify_content
-      alignItems = self.parent.styles.align_items
-      index = self.index
+      # align-self overrides align-items for individual items
+      alignItems = self.styles.align_self or self.parent.styles.align_items
 
-      changed = []
-
-      justifyContent = self.styles.align_self or justifyContent if parentFlexDirection == "column" else justifyContent
-      alignItems = self.styles.align_self or alignItems if parentFlexDirection == "row" else alignItems
+      # justify-content controls distribution along the MAIN axis (flow direction)
+      # align-items controls alignment along the CROSS axis (perpendicular to flow)
       
-      if justifyContent == "center":
-        if parentFlexDirection == "row": x = x + ((parent_w//2) - (self.parent.saved_width//2))
-        else: x = parent_x + (parent_w - my_w) // 2
-
-      if justifyContent == "flex-end":
-        if parentFlexDirection == "row": x = x + (parent_w - self.parent.saved_width) - pt - pb
-        else: x = parent_x + (parent_w - my_w) - pr
-
-      if justifyContent == "space-between":
-        if parentFlexDirection == "row":
-          if len(self.parent.children) > 1: x = x + (index * ((parent_w - self.parent.saved_width - pl - pr) // (len(self.parent.children) - 1)))
-        else:
-          if len(self.parent.children) > 1: y = y + (index * ((parent_h - self.parent.saved_height - pt - pb) // (len(self.parent.children) - 1)))
-          changed.append("space-between")
-
-      if alignItems == "center":
-        if parentFlexDirection == "column": y = y + ((parent_h//2) - (self.parent.saved_height//2))
-        else: y = parent_y + (parent_h - my_h) // 2
-        changed.append("align-items-center")
-
-      if alignItems == "flex-end":
-        if parentFlexDirection == "column": y = y + (parent_h - self.parent.saved_height) - pt - pb
-        else: y = parent_y + (parent_h - my_h) - pb
-        changed.append("align-items-flex-end")
-
-    if self.__ID__ == "div1": print(y, changed)
+      if parentFlexDirection == "row":
+        # Main axis = X (horizontal), Cross axis = Y (vertical)
+        
+        # justify-content controls X positioning (main axis)
+        if justifyContent == "center":
+          x = x + ((parent_w // 2) - (self.parent.saved_width // 2))
+        elif justifyContent == "flex-end":
+          x = x + (parent_w - self.parent.saved_width) - pl - pr
+        elif justifyContent == "space-between":
+          if len(self.parent.children) > 1:
+            x = x + (self.index * ((parent_w - self.parent.saved_width - pl - pr) // (len(self.parent.children) - 1)))
+        
+        # align-items controls Y positioning (cross axis)
+        if alignItems == "center":
+          y = parent_y + (parent_h - my_h) // 2
+        elif alignItems == "flex-end":
+          y = parent_y + (parent_h - my_h) - pb
+        elif alignItems == "flex-start":
+          y = parent_y + pt
+          
+      else:  # column
+        # Main axis = Y (vertical), Cross axis = X (horizontal)
+        
+        # justify-content controls Y positioning (main axis)
+        if justifyContent == "center":
+          y = y + ((parent_h // 2) - (self.parent.saved_height // 2))
+        elif justifyContent == "flex-end":
+          y = y + (parent_h - self.parent.saved_height) - pt - pb
+        elif justifyContent == "space-between":
+          if len(self.parent.children) > 1:
+            y = y + (self.index * ((parent_h - self.parent.saved_height - pt - pb) // (len(self.parent.children) - 1)))
+        
+        # align-items controls X positioning (cross axis)
+        if alignItems == "center":
+          x = parent_x + (parent_w - my_w) // 2
+        elif alignItems == "flex-end":
+          x = parent_x + (parent_w - my_w) - pr
+        elif alignItems == "flex-start":
+          x = parent_x + pl
 
     return (x, y)
   
@@ -102,7 +113,7 @@ class Layout:
   def setSize(self, width = None, height = None):
     self.saved_width = width
     self.saved_height = height
-    
+
     if self.styles.mode == LAYOUT_MODES["FIXED"]:
       return
     
